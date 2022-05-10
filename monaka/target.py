20a6988f7fafcb9a747e3f717709b9a092cf099e
yuuki-yamaguchi <yuukiyamaguchi5@gmail.com>
2022-05-10 22:18:40 +0900

    create empty classes

---
+++ monaka/target.py
@@ -0,0 +1,15 @@
+class Target():
+    def __init__(self):
+        pass
+
+class Target1TokenSeq(Target):
+    pass
+
+class Target2TokenSeq(Target):
+    pass
+
+class Target1Sent(Target1TokenSeq):
+    pass
+
+class Target2Sent(Target2TokenSeq):
+    pass
